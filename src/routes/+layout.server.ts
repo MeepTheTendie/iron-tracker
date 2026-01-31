f3b9304cfafec0ec917e6ef4067317bda6850984
MeepTheTendie <wesleympennock@gmail.com>
2026-01-31 09:47:59 -0600

    Fix auth import in layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,8 @@
 import type { LayoutServerLoad } from "./$types";
-import { auth } from "$lib/server/auth";
+import { getSession } from "$lib/server/auth";
 
 export const load: LayoutServerLoad = async (event) => {
-  const session = await auth.api.getSession({
-    headers: event.request.headers,
-  });
+  const session = await getSession(event.request.headers);
 
   return {
     isAuthenticated: !!session?.user,
